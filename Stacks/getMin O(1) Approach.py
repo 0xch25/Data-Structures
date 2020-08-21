929cb7344aa046c9c39ddcaac8d31a0cf85fb114
Chandan S <48490869+0xch25@users.noreply.github.com>
2020-08-21 08:51:33 +0530

    print min element on every push operation

--- Stacks/getMin O(1) Approach.py
+++ Stacks/getMin O(1) Approach.py
@@ -6,6 +6,7 @@ class Stack:
         if len(stk)==0 and len(minstk)==0:
             minstk.append(data)
             stk.append(data)
+            print("min element:",minstk[-1])
 
         else:
             if data<minstk[0]:
@@ -13,6 +14,8 @@ class Stack:
                 minstk.append(data)
             else:
                 stk.append(data)
+            print("min element:", minstk[-1])
+
     def pop(self):
         x=stk.pop()
         if x==minstk[-1]:
@@ -20,12 +23,16 @@ class Stack:
         return x
     def getMin(self):
         return minstk[-1]
+
 s=Stack()
-s.push(10)
-s.push(20)
-s.push(30)
-print(s.getMin())
+s.push(3)
 s.push(5)
 print(s.getMin())
+s.push(2)
+s.push(1)
+print(s.getMin())
 s.pop()
+print(s.getMin())
+s.pop()
+print(stk[-1])
 print(s.getMin())
