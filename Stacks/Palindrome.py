a1f35aef0c62808e1bd6482488f8111b8f0b9a7e
Chandan S <48490869+0xch25@users.noreply.github.com>
2020-08-21 08:50:55 +0530

    function added to check weather the given string is palindrome or not

---
+++ Stacks/Palindrome.py
@@ -0,0 +1,15 @@
+def palindrome(str):
+    n = len(str) - 1
+    x = int(n / 2)
+    str1 = str[0:x]
+    print("first half:", str1)
+    str2 = str[:x:-1]
+    print("second half:", str2)
+    if str1 == str2:
+        print("palindrome")
+    else:
+        print("not palindrome")
+
+
+palindrome("abababbbabbXabbbabbabbb")  # Not palindrome
+palindrome("ababXbaba") #palindrome
