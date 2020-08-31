f831cf51a81eac14d4aa899cbba5ca7c3076c539
ch25h1 <66072219+ch25h1@users.noreply.github.com>
2020-08-31 09:41:09 +0530

    added new files

--- Arrays/Sort Array By Parity.py
+++ Arrays/Sort Array By Parity.py
@@ -5,6 +5,12 @@ Example 1:
 Input: [3,1,2,4]
 Output: [2,4,3,1]
 The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.'''
+
+# ****OPTIMIZED****
+def sortArrayByParityV1(A):
+    return [s for s in A if s%2==0]+[s for s in A if s%2!=0]
+
+#Basic
 def sortArrayByParity(A):
     B = []
     for num in A:
@@ -21,3 +27,4 @@ def sortArrayByParityV2(A):
 A=[1,2,3,4]
 print(sortArrayByParity(A))
 print(sortArrayByParityV2(A))
+print(sortArrayByParityV1(A))
