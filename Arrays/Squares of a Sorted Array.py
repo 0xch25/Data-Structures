2a1562a0823e572c70f5c6779120adb0226a3e6e
ch25h1 <66072219+ch25h1@users.noreply.github.com>
2020-08-31 09:51:33 +0530

    added new files

--- Arrays/Squares of a Sorted Array.py
+++ Arrays/Squares of a Sorted Array.py
@@ -1,3 +1,12 @@
+'''Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
+Example 1:
+Input: [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+One Line Solution:
+                     def sortedSquares(self, A: List[int]) -> List[int]:
+                        return sorted([x**2 for x in A])
+
+'''
 def sortedSquares(A):
     B = []
     for i in A:
