85c8a88b27dfbe79841a8028bb5cc21b326d4fe7
ch25h1 <66072219+ch25h1@users.noreply.github.com>
2020-09-03 12:20:30 +0530

    added new Files

--- Linked List/Reverse a Linked List.py
+++ Linked List/Reverse a Linked List.py
@@ -3,6 +3,16 @@ Reverse a singly linked list.
 Example:
 Input: 1->2->3->4->5->NULL
 Output: 5->4->3->2->1->NULL
+Approach1:
+    def Reverse(self):
+        prev = None
+        current = self.head
+        while current:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        self.head = prev
 '''
 class Node:
     def __init__(self, data=None, next=None):
@@ -31,16 +41,17 @@ class LinkedList:
             current = current.next
         print()
 
-
-    def Reverse(self):
-        prev = None
+    def Reverse2(self):
         current = self.head
+        previous, nextnode = None, None
         while current:
-            next = current.next
-            current.next = prev
-            prev = current
-            current = next
-        self.head = prev
+            nextnode = current.next
+            current.next = previous
+
+            previous = current
+            current = nextnode
+        self.head=previous
+
 
 L = LinkedList()
 L.insert(3)
@@ -49,5 +60,5 @@ L.insert(0)
 L.insert(4)
 L.insert(5)
 L.print(L.head)
-L.Reverse()
+L.Reverse2()
 L.print(L.head)
