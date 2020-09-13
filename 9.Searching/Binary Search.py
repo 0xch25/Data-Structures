a574bbb216528e18460143f93c7244f37ac17709
Chandan S <48490869+0xch25@users.noreply.github.com>
2020-09-13 21:28:14 +0530

    Added New Files

---
+++ 9.Searching/Binary Search.py
@@ -0,0 +1,22 @@
+'''
+Implementation of Binary Search Iteratively
+'''
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+    return -1
+arr = [1,12,13,2,3,4,5,6,10,40]
+x = 4
+result = binary_search(arr, x)
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
