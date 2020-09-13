c2bdaac39bc2ba75db2dde7d765879642342bdce
Chandan S <48490869+0xch25@users.noreply.github.com>
2020-09-13 09:11:29 +0530

    Added New Files

--- 8.Sorting/Bubble Sort(Improved).py
+++ 8.Sorting/Bubble Sort(Improved).py
@@ -1,14 +1,18 @@
 '''
-Implementation of Bubble sort.
+Implementation of Modified Bubble sort.
 
 '''
 def Bubblesort(lst):
     for i in range(n-1):
+        swapped=False
         for k in range(n-i-1):
             if lst[k]>lst[k+1]:
                 temp=lst[k]
                 lst[k]=lst[k+1]
                 lst[k+1]=temp
+                swapped=True
+        if swapped==False:
+            break
     return lst
 
 n=int(input("Enter The No of Elements:"))
