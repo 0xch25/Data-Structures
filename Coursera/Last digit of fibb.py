52ef50c6548e9487f1b112ef3e2e3f2a5b97ab9d
Chandan S <>
2020-11-04 10:59:50 +0530

    Added new file

--- Coursera/Last digit of fibb.py
+++ Coursera/Last digit of fibb.py
@@ -1,7 +1,7 @@
 def fiblist(n):
     fib=[0,1]
     for i in range(2,n+1):
-        fib.append(fib[i-1]+fib[i-2])
-    return fib[n]%10
+        fib.append((fib[i-1]+fib[i-2])%10)
+    return fib[n]
 n=int(input())
 print(fiblist(n))
