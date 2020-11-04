ee6ceb33590eec75ab3a2e887805e599fd30248e
Chandan S <>
2020-11-04 10:49:59 +0530

    Added new file

--- Coursera/LCM Naive.py
+++ Coursera/LCM Naive.py
@@ -1,7 +1,16 @@
+def gcd_euclid(a, b):
+    dividend = a if (a >= b) else b
+    divisor = a if (a <= b) else b
+
+    while divisor != 0:
+        remainder = dividend % divisor
+        dividend = divisor
+        divisor = remainder
+
+    return dividend
+
+
 def lcm(a, b):
-    for l in range(1, a * b + 1):
-        if l % a == 0 and l % b == 0:
-            return l
-    return a * b
+    return (a * b) // gcd_euclid(a, b)
 a,b=map(int,input().split())
 print(lcm(a,b))
